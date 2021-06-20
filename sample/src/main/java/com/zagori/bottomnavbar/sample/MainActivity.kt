69fd50bdfb9744cfceaa7d290b965a332a73abf3
yousseflabihi <you.labihi@gmail.com>
2021-06-21 02:42:36 +0800

    Switch to viw binding in the sample

--- sample/src/main/java/com/zagori/bottomnavbar/sample/MainActivity.kt
+++ sample/src/main/java/com/zagori/bottomnavbar/sample/MainActivity.kt
@@ -2,40 +2,39 @@ package com.zagori.bottomnavbar.sample
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.zagori.bottomnavbar.BottomNavBar
 import com.zagori.bottomnavbar.BottomNavBar.OnBottomNavigationListener
+import com.zagori.bottomnavbar.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val textMessage: TextView = findViewById(R.id.message)
-        val bottomNavView = findViewById<BottomNavBar>(R.id.bottom_nav_view)
+        setContentView(binding.root)
 
-        bottomNavView.setBottomNavigationListener(object: OnBottomNavigationListener{
+        binding.bottomNavView.setBottomNavigationListener(object: OnBottomNavigationListener{
             override fun onNavigationItemSelected(menuItem: MenuItem?): Boolean {
                 return when (menuItem!!.itemId) {
                     R.id.navigation_home -> {
-                        textMessage.setText(R.string.title_home)
+                        binding.message.setText(R.string.title_home)
                         true
                     }
                     R.id.navigation_payment -> {
-                        textMessage.setText(R.string.title_payment)
+                        binding.message.setText(R.string.title_payment)
                         true
                     }
                     R.id.navigation_new_cart -> {
-                        textMessage.setText(R.string.title_new_cart)
+                        binding.message.setText(R.string.title_new_cart)
                         true
                     }
                     R.id.navigation_dashboard -> {
-                        textMessage.setText(R.string.title_dashboard)
+                        binding.message.setText(R.string.title_dashboard)
                         true
                     }
                     R.id.navigation_notifications -> {
-                        textMessage.setText(R.string.title_notifications)
+                        binding.message.setText(R.string.title_notifications)
                         true
                     }
                     else -> false
