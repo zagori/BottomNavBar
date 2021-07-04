0a2981a12b0bcc96b38f13f8c55e545541c30c45
yousseflabihi <you.labihi@gmail.com>
2021-07-04 23:30:35 +0800

    Change fab size attribute to enum. Refactor BottomNavBar. Set default values for some attributes

--- bottomnavbar/src/main/java/com/zagori/bottomnavbar/BottomNavBar.kt
+++ bottomnavbar/src/main/java/com/zagori/bottomnavbar/BottomNavBar.kt
@@ -1,19 +1,16 @@
 package com.zagori.bottomnavbar
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.content.res.Resources.NotFoundException
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
-import androidx.annotation.ColorInt
-import androidx.annotation.IdRes
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.zagori.bottomnavbar.databinding.BottomNavBarBinding
 
@@ -40,49 +37,62 @@ class BottomNavBar(context: Context, attrs: AttributeSet?) : CoordinatorLayout(c
         fun onNavigationItemSelected(menuItem: MenuItem?): Boolean
     }
 
-    companion object {
-        private val TAG = BottomNavBar::class.java.simpleName
-    }
+    private var menuResID: Int
+    private var menuItemColorState: ColorStateList? = null
+    private var navBackgroundColor: Int
+
+    private var curveMargin: Float
+    private var roundedCornerRadius: Float
+    private var cradleVerticalOffset: Float
+
+    private var fabMenuIndex: Int
+    private var fabBackgroundColor: ColorStateList? = null
+    private var fabIconColor: ColorStateList? = null
+    private var bnFabSize: Int
+
+    private val fabDiameter: Float
+        get() = when (bnFabSize) {
+            1 -> resources.getDimension(R.dimen.fab_size_mini)
+            else -> resources.getDimension(R.dimen.fab_size_normal)
+        }
+
 
     init {
         addView(binding.root)
 
-        val styleAttrs = context.theme.obtainStyledAttributes(attrs, R.styleable.BottomNavBar, 0, 0)
-        val curveMargin = styleAttrs.getDimension(R.styleable.BottomNavBar_bn_curve_margin, 0f)
-        val roundedCornerRadius =
-            styleAttrs.getDimension(R.styleable.BottomNavBar_bn_curve_rounded_corner_radius, 0f)
-        val cradleVerticalOffset =
-            styleAttrs.getDimension(R.styleable.BottomNavBar_bn_curve_vertical_offset, 0f)
-        @ColorInt val backgroundColor = styleAttrs.getColor(
-            R.styleable.BottomNavBar_bn_background_color,
-            binding.navView.solidColor
-        )
-        @IdRes val menuResID = styleAttrs.getResourceId(R.styleable.BottomNavBar_bn_menu, -1)
-        val fabMenuIndex = styleAttrs.getInt(R.styleable.BottomNavBar_bn_fab_menu_index, -1)
-        val menuItemColorState =
-            styleAttrs.getColorStateList(R.styleable.BottomNavBar_bn_item_color)
-        val fabBackgroundColor =
-            styleAttrs.getColorStateList(R.styleable.BottomNavBar_bn_fab_background_color)
-        val fabIconColor = styleAttrs.getColorStateList(R.styleable.BottomNavBar_bn_fab_icon_color)
-        val fabDiameter: Float
-        val fabSize: Int
-        val bnFabSize = styleAttrs.getString(R.styleable.BottomNavBar_bn_fab_size)
-        if (bnFabSize == null || bnFabSize.equals("normal", ignoreCase = true)) {
-            // set fab param to default fab parameters
-            fabDiameter = resources.getDimension(R.dimen.fab_size_normal)
-            fabSize = FloatingActionButton.SIZE_NORMAL
-        } else if (bnFabSize.equals("mini", ignoreCase = true)) {
-            fabDiameter = resources.getDimension(R.dimen.fab_size_mini)
-            fabSize = FloatingActionButton.SIZE_MINI
-        } else {
-            Log.e(TAG, "bnFabSize should be either 'mini' or 'normal'")
-            throw IllegalArgumentException("bnFabSize '$bnFabSize' does not match any of the fab size values")
-        }
+        val attributes = context.theme.obtainStyledAttributes(attrs, R.styleable.BottomNavBar, 0, 0)
 
-        // check if no menu has been added to BottomNav view
-        if (menuResID == -1) {
-            Log.e(TAG, "menuResID should not be null.")
-            throw NotFoundException("Menu must be added to BottomNav.")
+        try {
+            menuResID = attributes.getResourceId(R.styleable.BottomNavBar_bn_menu, -1)
+            menuItemColorState =
+                attributes.getColorStateList(R.styleable.BottomNavBar_bn_item_color)
+            navBackgroundColor = attributes.getColor(
+                R.styleable.BottomNavBar_bn_background_color,
+                binding.navView.solidColor
+            )
+
+            curveMargin = attributes.getDimension(R.styleable.BottomNavBar_bn_curve_margin, 0f)
+            roundedCornerRadius =
+                attributes.getDimension(R.styleable.BottomNavBar_bn_curve_rounded_corner_radius, 0f)
+            cradleVerticalOffset =
+                attributes.getDimension(R.styleable.BottomNavBar_bn_curve_vertical_offset, 0f)
+
+            fabMenuIndex = attributes.getInt(R.styleable.BottomNavBar_bn_fab_menu_index, 0)
+            fabBackgroundColor =
+                attributes.getColorStateList(R.styleable.BottomNavBar_bn_fab_background_color)
+            fabIconColor = attributes.getColorStateList(R.styleable.BottomNavBar_bn_fab_icon_color)
+            bnFabSize = attributes.getInt(R.styleable.BottomNavBar_bn_fab_size, -1)
+        } finally {
+            attributes.recycle()
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        // check if no menu has been added
+        if (menuResID == -1){
+            throw NotFoundException("Menu must be added to BottomNav. menuResID should not be null.")
         }
 
         // add the menu to the bottom-nav
@@ -102,28 +112,20 @@ class BottomNavBar(context: Context, attrs: AttributeSet?) : CoordinatorLayout(c
             binding.navView.selectedItemId = binding.navView.menu.getItem(fabMenuIndex).itemId
         }
 
-        // check if fab is not added
-        if (fabMenuIndex == -1) {
-            binding.fab.visibility = GONE
-            Log.w(TAG, "No fab is added")
-        }
-
         // check if fab position does not match any menu item position
-        if (fabMenuIndex >= binding.navView.menu.size()) {
-            binding.fab.visibility = GONE
-            Log.e(
-                TAG,
-                "fabMenuIndex is out of bound. fabMenuIndex only accepts values from 0 to " + (binding.navView.menu.size() - 1)
-            )
-            throw IllegalArgumentException("fabMenuIndex does not match any menu item position")
-        }
+        if (fabMenuIndex >= binding.navView.menu.size()) throw IllegalArgumentException(
+            "ATTRIBUTE OUT OF BOUND",
+            Throwable("The attribute bn_fab_menu_index does not match any menu item position. bn_fab_menu_index only accepts values from 0 to ${binding.navView.menu.size() - 1}")
+        )
 
         // Set up fab
         binding.fab.apply {
-            visibility = VISIBLE // show fab
-            size = fabSize // set fav size
+            size = bnFabSize // set fav size
             backgroundTintList = fabBackgroundColor // set the fab background tint
             imageTintList = fabIconColor // set the fab icon tint
+
+            // get the icon from menu item and set it to the fab
+            setImageDrawable(binding.navView.menu.getItem(fabMenuIndex).icon)
         }
 
         val topEdge = TopEdgeTreatment(
@@ -135,17 +137,14 @@ class BottomNavBar(context: Context, attrs: AttributeSet?) : CoordinatorLayout(c
         ).also { it.setFabDiameter(fabDiameter) }
 
         ViewCompat.setBackground(binding.navView, MaterialShapeDrawable().apply {
-            setTint(backgroundColor)
+            setTint(navBackgroundColor)
             shapeAppearanceModel = shapeAppearanceModel.toBuilder().setTopEdge(topEdge).build()
         })
-
-        // get the icon from menu item and set it to the fab
-        binding.fab.setImageDrawable(binding.navView.menu.getItem(fabMenuIndex).icon)
 
         // remove the icon from the item in the bottom-nav
         binding.navView.menu.getItem(fabMenuIndex).icon = null
 
-        // get an instance of the 1st menu item, as we need it to
+        // get an instance of the 1st menu item
         val bottomNavigationItemView = (binding.navView.getChildAt(0)
                 as BottomNavigationMenuView).getChildAt(1) as BottomNavigationItemView
 
@@ -166,5 +165,7 @@ class BottomNavBar(context: Context, attrs: AttributeSet?) : CoordinatorLayout(c
                 binding.fab.requestLayout()
             }
         })
+
+        invalidate()
     }
 }
